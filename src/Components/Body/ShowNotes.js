730ee8421a1e41ab97927de2ff55e95ac067b07c
dhanapalms <sdhanapalms@gmail.com>
2021-08-07 23:59:49 +0530

    Modified ShowNotes title

--- src/Components/Body/ShowNotes.js
+++ src/Components/Body/ShowNotes.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 const ShowNotes=(props)=>{
     return(
         <Card>
+            <h3>Notes:</h3>
         {props.notes.map((note)=><h2 key={note.id}>{note.title}</h2>)}
         </Card>
     );
