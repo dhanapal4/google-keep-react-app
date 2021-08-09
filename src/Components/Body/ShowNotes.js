0aeefecc05ae367799463b13f4d00801e79b6d08
dhanapalms <sdhanapalms@gmail.com>
2021-08-09 12:03:56 +0530

    View Note bug fixed

--- src/Components/Body/ShowNotes.js
+++ src/Components/Body/ShowNotes.js
@@ -4,24 +4,34 @@ import classes from "./ShowNotes.module.css";
 import UpdateNote from "./UpdateNote";
 
 const ShowNotes = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-const onCloseHandler=()=>{
-  setIsOpen(false);
-}
-
+  const [viewNote, setViewNote] = useState({
+    open: false,
+    note: {},
+  });
+  const onOpenHandler = (note) => {
+    console.log(`${note.title}`);
+    setViewNote({ open: true, note:note });
+  };
+  const onCloseHandler = () => {
+    setViewNote({ open: false, note: {} });
+  };
 
   return (
     <div className={classes.bkg}>
       {props.notes.map((note) => (
-        <Card key={note.id}>
-          {isOpen && <UpdateNote note={note} onClose={onCloseHandler}/>}
-
-          <div className={classes.note} onClick={() => setIsOpen(true)}>
-            <h3>{note.title}</h3>
-            <h6>{note.body}</h6>
-          </div>
-        </Card>
+          <Card key={note.id}>
+            <div key={note.id}
+              className={classes.note}
+              onClick={onOpenHandler.bind( null,note)}
+            >
+              <h3>{note.title}</h3>
+              <h6>{note.body}</h6>
+            </div>
+          </Card>
       ))}
+      <UpdateNote viewNote={viewNote} onClose={onCloseHandler}>
+        {/* <h3>Hey</h3> */}
+      </UpdateNote>
     </div>
   );
 };
--- src/Components/Body/UpdateNote.js
+++ src/Components/Body/UpdateNote.js
@@ -5,14 +5,17 @@ import classes from './UpdateNote.module.css';
 
 
 const UpdateNote=(props)=>{
-    console.log('modal')
-    console.log(`${props.note.title}`);
+    const note=props.viewNote.note;
+    console.log('clicked...')
+    if(!props.viewNote.open){
+        return null;
+    }
     return reactDom.createPortal(
         <>
         <div className={classes.overlay} onClick={props.onClose}></div>
         <div className={classes.modal}>
-            <h3>{props.note.title}</h3>
-            <h5>{props.note.body}</h5>
+            <h3>{note.title}</h3>
+            <h5>{note.body}</h5>
             <div className={classes.close}><p onClick={props.onClose}>Close</p></div>
         </div>
         </>,document.getElementById('portal')
