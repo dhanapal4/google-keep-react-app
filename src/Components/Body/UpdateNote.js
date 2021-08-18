21bbea6c07f57f2c31f5313aaf4bc8ce24e64dd2
dhanapalms <sdhanapalms@gmail.com>
2021-08-18 22:52:09 +0530

    Few UI responsive design worked

--- src/Components/Body/UpdateNote.js
+++ src/Components/Body/UpdateNote.js
@@ -1,23 +1,30 @@
+import React from "react";
+import reactDom from "react-dom";
+import classes from "./UpdateNote.module.css";
 
-import React from 'react';
-import reactDom from 'react-dom';
-import classes from './UpdateNote.module.css';
+const UpdateNote = (props) => {
+  const note = props.viewNote.note;
+  if (!props.viewNote.open) {
+    return null;
+  }
+  const noteDate=note.timestamp.split(" ");
+  console.log(noteDate)
+  const lastUpdateDate=noteDate[2]+noteDate[1]+", "+noteDate[3]+" "+noteDate[4];
+  return reactDom.createPortal(
+    <>
+      <div className={classes.overlay} onClick={props.onClose}></div>
+      <div className={classes.modal}>
+        <h3>{note.title}</h3>
+        <h5>{note.body}</h5>
 
-
-const UpdateNote=(props)=>{
-    const note=props.viewNote.note;
-    if(!props.viewNote.open){
-        return null;
-    }
-    return reactDom.createPortal(
-        <>
-        <div className={classes.overlay} onClick={props.onClose}></div>
-        <div className={classes.modal}>
-            <h3>{note.title}</h3>
-            <h5>{note.body}</h5>
-            <div className={classes.close}><p onClick={props.onClose}>Close</p></div>
+        <h6>Last updated :{lastUpdateDate}</h6>
+        <div className={classes.close}>
+          <p onClick={props.onClose}>Close</p>
         </div>
-        </>,document.getElementById('portal')
-    );}
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+};
 
-    export default UpdateNote;
+export default UpdateNote;
